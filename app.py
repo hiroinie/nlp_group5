2d41d5bac03d07183078673229eeeac19f358994
hiroinie <hiromitsu.fujiyama@student.ie.edu>
2025-06-18 18:53:54 +0200

    Add sentiment analysis

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from slide_4p import run as run_4p
 from slide_financial import run as run_financial
+from sentiment_analysis import run as run_sentiment
 
 st.set_page_config(page_title="AI Slide Generator")
 
 st.sidebar.title("Slide Type")
-page = st.sidebar.selectbox("Choose the slide to generate", ["4P Analysis", "Company Financial"])
+page = st.sidebar.selectbox("Choose the slide to generate", ["4P Analysis", "Company Financial",  "Company News Sentiment"])
+
 
 if page == "4P Analysis":
     run_4p()
-else:
+elif page == "Company Financial":
     run_financial()
+elif page == "Company News Sentiment":
+    run_sentiment()
